405173bf1fd4e2da81a5ab02704bad25f322044f
Dean Shelton <Dean.X.Shelton.-ND@disney.com>
2016-03-13 09:37:20 -0700

    initial

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  context: __dirname + "/app",
+  entry: {
+  javascript: "./app.js",
+  html: "./index.html",
+},
+  module: {
+  loaders: [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loaders: ["react-hot", "babel-loader"],
+    },
+    {
+      test: /\.html$/,
+      loader: "file?name=[name].[ext]",
+    },
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: ["babel"],
+      query:
+      {
+        presets:["es2015", "react"]
+      }
+    }
+  ],
+},
+  output: {
+    filename: "app.js",
+    path: __dirname + "/dist",
+  },
+}
